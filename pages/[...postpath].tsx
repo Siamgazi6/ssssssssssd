f921b5504fef32127fcf1515dd12f305d2a898f9
Siamgazi6 <89618597+Siamgazi6@users.noreply.github.com>
2026-08-24 14:34:50 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://blogverse.website/` 
+					`https://t.co/mjqBYiANM2` 
 				}`,
 			},
 		};
